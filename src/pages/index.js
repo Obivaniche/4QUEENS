11ff0849679f0a74a7f074c16bf6d8c418888329
Vladislav Smirnov <obivaniche@yandex.ru>
2025-07-11 00:41:10 +0300

    bugfix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 const searchButton = document.querySelector('.header__button_search');
 if (searchButton) {
     const searchInput = document.querySelector('.header__label');
+    const inputHeader = document.querySelector('.header__input');
     const content = document.querySelector('.content');
     const body = document.querySelector('.body')
     searchButton.addEventListener('click', function (e) {
         searchInput.classList.toggle('header__label_active');
         content.classList.toggle('content_lock');
         body.classList.toggle('body_lock');
+        inputHeader.classList.toggle('header__input_active');
     });
 };
 
