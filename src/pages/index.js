c585788264352e39f674fa28b0e6bc1527117840
Vladislav Smirnov <obivaniche@yandex.ru>
2025-07-10 22:36:25 +0300

    product

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,3 @@
-import '../pages/index.css';
-
 const searchButton = document.querySelector('.header__button_search');
 if (searchButton) {
     const searchInput = document.querySelector('.header__label');
@@ -15,7 +13,23 @@ if (searchButton) {
 const closePromo = document.querySelector('.header__promo-close-button');
 if (closePromo) {
     const headerPromo = document.querySelector('.header__promo');
-    closePromo.addEventListener('click', function(e) {
+    closePromo.addEventListener('click', function (e) {
         headerPromo.classList.add('header__promo_hide');
     });
 };
+
+const buttonLike = document.querySelector('.product__button-like')
+buttonLike.addEventListener('click', function (e) {
+    buttonLike.classList.toggle('product__button-like_active');
+});
+
+const openTextElements = document.querySelectorAll('.information__arrow');
+if (openTextElements.length > 0) {
+    openTextElements.forEach(function (openText) {
+        const articleText = openText.closest('.information__article').querySelector('.information__article-text');
+        openText.addEventListener('click', function (e) {
+            openText.classList.toggle('information__arrow_active');
+            articleText.classList.toggle('information__article-text_active');
+        });
+    });
+}
